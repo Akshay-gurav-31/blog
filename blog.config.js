1b298c232a605a4718819d900b88736ee0e85895
Akshay Gurav <mathsjourneywithakshay@gmail.com>
2025-07-09 15:21:09 +0530

    Initial commit

--- blog.config.js
+++ blog.config.js
@@ -1,6 +1,6 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
+  title: 'Yesha Writes',
+  author: 'Yesha Parwani',
   email: 'i@craigary.net',
   link: 'https://nobelium.vercel.app',
   description: 'This gonna be an awesome website.',
